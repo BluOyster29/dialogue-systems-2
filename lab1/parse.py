d86f5f1fab4b166c13682b053a02bf1d84c556d7
Rob <gusroberth@student.gu.se>
2019-09-06 14:17:48 +0200

    updates

--- lab1/parse.py
+++ lab1/parse.py
@@ -1,15 +1,15 @@
 from rasa_nlu.model import Interpreter
 import json
 interpreter = Interpreter.load("./models/current/nlu")
-messages = ["hello there", "howdy", "alright mate", "what's happening"]
+messages = ["hello there", "howdy", "alright mate", "what's happening", "ayup"]
 
-def parse_messages(message):
-	for i in messages:
-		result = interpreter.parse(i)
+def parse_messages(messages):
+	for message in messages:
+		result = interpreter.parse(message)
 		print(json.dumps(result, indent=2))
 
 	
-parse_messages(message)
+parse_messages(messages)
 
 
 
