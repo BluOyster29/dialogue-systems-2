975cc3c2490265de9c07c8f1893678f557ad4f3d
Robert <gusroberth@student.gu.se>
2019-09-28 12:12:41 +0200

    m

--- lab2/call_john_ddd_2019/call_john/device.py
+++ lab2/call_john_ddd_2019/call_john/device.py
@@ -2,7 +2,7 @@ from tdm.lib.device import DddDevice, DeviceAction, DeviceWHQuery, Validity
 
 
 class CallJohnDevice(DddDevice):
-
+    '''
     class PhoneNumberAvailable(Validity):
         def is_valid(self, select_contact):
             contact = self.device.CONTACTS.get(select_contact)
@@ -10,17 +10,20 @@ class CallJohnDevice(DddDevice):
                 return False
             else:
                 return True
-
+    '''
     class MakeCall(DeviceAction):
         def perform(self, contact_name, number_type):
-            #contact = self.device.CONTACTS.get(select_contact)
-            #type = self.device.CONTACTS.get(select_number)
+            contact = self.device.CONTACTS.get(select_contact)
+            number_type = self.device.CONTACTS.get(select_number)
             return True
+
+    '''
 
     class contact_lookup(DeviceWHQuery):
         def perform(self, select_contact, select_number):
             number = self.device.PHONE_NUMBERS.get(select_contact)
             return [number]
+    '''
 
     JOHN = "contact_john"
     LISA = "contact_lisa"
